8dd90c2a5cd9950ebf555403cec6eb26ea1da17a
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-07-28 22:22:24 +0600

    complete the hidden part & update char remove part

--- script.js
+++ script.js
@@ -7,12 +7,50 @@ randomnumber.addEventListener('click',function(){
 
 function display(userInput){
    if(userInput==='C'){
+    //clear all char at a time
     document.getElementById('userInputDisplay').value="";
+
    }else if(userInput=='<'){
-    let displayValue=document.getElementById('userInputDisplay');
-    document.getElementById('userInputDisplay').value=displayValue.value.substring(0,displayValue.value.length-1)
+
+    if(document.getElementById('userInputDisplay').value==""){
+        swal("Input field is empty!", "...Nothing to remove")
+    }
+    else{
+        //clear char one by one from right side
+        let displayValue=document.getElementById('userInputDisplay');
+        document.getElementById('userInputDisplay').value=displayValue.value.substring(0,displayValue.value.length-1)
+         //str.substring(0,str.length-1)
+    }
    }
    else{
     document.getElementById('userInputDisplay').value+=userInput;
    }  
+}
+
+const submit=document.getElementById('submit');
+submit.addEventListener('click',function(){
+    let displayValue=document.getElementById('userInputDisplay').value;
+   
+    if(displayValue==""){
+        swal("Input field is empty");
+        document.getElementById("notMatch").style.display='none';
+        document.getElementById("match").style.display='none';
+    }
+    else{
+        let randomNumber=document.getElementById('randomNumberDisplay').value;
+
+        if(displayValue==randomNumber){
+            document.getElementById("match").style.display='block';
+            document.getElementById("notMatch").style.display='none';
+        }else{
+            displayProperty('notMatch','block');
+            displayProperty('match','none')
+           
+    }
+    }
+  
+})
+
+function displayProperty(id,value){
+    document.getElementById(id).style.display=value;
 }
