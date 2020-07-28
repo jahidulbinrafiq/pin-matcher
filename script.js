d07ba7312b82a44e3b4347365dcf63f18cc4df31
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-07-28 23:07:01 +0600

    update design update & add another remove section

--- script.js
+++ script.js
@@ -10,14 +10,21 @@ function display(userInput){
     //clear all char at a time
     document.getElementById('userInputDisplay').value="";
 
-   }else if(userInput=='<'){
+   }else if(userInput=='<' || userInput=='>'){
 
+    let displayValue=document.getElementById('userInputDisplay');
     if(document.getElementById('userInputDisplay').value==""){
         swal("Input field is empty!", "...Nothing to remove")
+        displayProperty('notMatch','none');
+        displayProperty('match','none')
+    }
+    else if(userInput=='>'){
+        //clear char one by one from left to right
+        document.getElementById('userInputDisplay').value=displayValue.value.substring(1,displayValue.value.length)
+         //str.substring(0,str.length)
     }
     else{
-        //clear char one by one from right side
-        let displayValue=document.getElementById('userInputDisplay');
+        //clear char one by one from right to left
         document.getElementById('userInputDisplay').value=displayValue.value.substring(0,displayValue.value.length-1)
          //str.substring(0,str.length-1)
     }
@@ -30,25 +37,22 @@ function display(userInput){
 const submit=document.getElementById('submit');
 submit.addEventListener('click',function(){
     let displayValue=document.getElementById('userInputDisplay').value;
+    let randomNumber=document.getElementById('randomNumberDisplay').value;
    
-    if(displayValue==""){
-        swal("Input field is empty");
-        document.getElementById("notMatch").style.display='none';
-        document.getElementById("match").style.display='none';
+    if((displayValue=="")||(randomNumber=="")){
+        swal("Field is empty!", "..Check Input Field & Random Number Field")
+        displayProperty('notMatch','none');
+        displayProperty('match','none')
     }
     else{
-        let randomNumber=document.getElementById('randomNumberDisplay').value;
-
         if(displayValue==randomNumber){
-            document.getElementById("match").style.display='block';
-            document.getElementById("notMatch").style.display='none';
+            displayProperty('match','block');
+            displayProperty('notMatch','none');
         }else{
             displayProperty('notMatch','block');
-            displayProperty('match','none')
-           
+            displayProperty('match','none')         
     }
-    }
-  
+    } 
 })
 
 function displayProperty(id,value){
