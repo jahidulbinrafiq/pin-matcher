840072a525f6ce11e0171a81408aa7135bf06af0
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-07-28 18:11:25 +0600

    userInput

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+//Generate pin section
 const randomnumber =document.getElementById('generatePin');
 randomnumber.addEventListener('click',function(){
     let generateNumber= Math.floor(1000 + Math.random() * 9000);
-    document.getElementById('randomNumberDisplay').value=generateNumber;
+    document.getElementById('randomNumberDisplay').value=generateNumber;   
 });
+
+function display(userInput){
+   if(userInput==='C'){
+    document.getElementById('userInputDisplay').value="";
+   }
+   else{
+    document.getElementById('userInputDisplay').value+=userInput;
+   }
+       
+}
+function remove(){
+    console.log(1);
+}
